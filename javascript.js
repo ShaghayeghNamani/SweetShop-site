1c5b17bb7bf7c0c5634e97ae3a9ce41e23461cbf
formylearn <anemone.business99@gmail.com>
2025-01-28 12:43:23 +0330

    Add ScrollReveal to project

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 const header = document.querySelector("header");
 
+// sticky header 
 window.addEventListener('scroll', function(){
     header.classList.toggle('sticky', window.scrollY > 80);
 })
 
+// navlist 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
@@ -16,3 +18,20 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 }
+
+// scroll reveal  
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: '2500',
+    reset: 'false'
+})
+ sr.reveal ('.home-text', {delay:200});
+ sr.reveal ('.home-img', {delay:300});
+ sr.reveal ('.container', {delay:300});
+
+ sr.reveal ('.about-img', {});
+ sr.reveal ('.about-text', {delay:200});
+
+ sr.reveal ('.middle-text', {});
+ sr.reveal ('.row-btn, .menu-content, .contact', {delay:200});
